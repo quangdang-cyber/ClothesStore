3e9abc331cfa9cbc822de7bb5a161bc02c54a890
Quang Dang <quang_dang@datahouse.com>
2022-12-02 04:18:44 +0700

    update fxml and login controller

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.clothesshop {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.clothesshop to javafx.fxml;
-    exports com.mycompany.clothesshop;
-}
